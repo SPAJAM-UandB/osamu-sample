0d5e384f2ae4202a9445559378c29eeeaf1fb659
中岡黎 <reideeplearning@gmail.com>
2020-08-29 00:19:31 +0900

    Initial Commit

---
+++ osamu-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  osamu-sample
+//
+//  Created by 中岡黎 on 2020/08/29.
+//  Copyright © 2020 中岡黎. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
